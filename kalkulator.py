30d2dfeac90c540649096ca3523b7c12877dd435
Joanna Swider <asia_tbg@tlen.pl>
2022-01-30 11:23:09 +0100

    Add kalkulator.py

---
+++ kalkulator.py
@@ -0,0 +1,30 @@
+
+import logging
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+def kalkulator (a, b, wybor):
+  if (a, b != str):
+    if wybor == "1":
+        logging.info(f"Dodaje {a} i {b}.")
+        return a + b
+    elif wybor == "2":
+        logging.info(f"Odejmuje {b} od {a}.")
+        return a - b
+    elif wybor == "3":
+        logging.info(f"Mnoze {a} i {b}.")
+        return a / b
+    elif wybor == "4":
+        logging.info(f"Dziele {a} przez {b}.")
+    else:
+        exit(1)
+  else:
+      exit(1)
+
+
+
+if __name__ == "__main__":
+    wybor = input("Podaj dzialanie, poslugujac sie odpowiednia liczba: 1 Dodawanie, 2 Odejmowanie, 3 Mnozenie, 4 Dzielenie:")
+    a = float(input("Podaj skladnik 1."))
+    b = float(input("Podaj skladnik 2."))
+   
+    print(f"Wynik to {kalkulator (a, b, wybor)}")
